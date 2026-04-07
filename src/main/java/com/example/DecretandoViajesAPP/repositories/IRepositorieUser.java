7580def18389ae05b62cafde89652ee2df76b3ee
sebastian <sebasmtz300@gmail.com>
2026-04-07 11:53:08 -0500

    feat Se crean nuevos servicios

--- src/main/java/com/example/DecretandoViajesAPP/repositories/IRepositorieUser.java
+++ src/main/java/com/example/DecretandoViajesAPP/repositories/IRepositorieUser.java
@@ -4,8 +4,12 @@ import com.example.DecretandoViajesAPP.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IRepositorieUser extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
 }
--- src/main/java/com/example/DecretandoViajesAPP/services/UserService.java
+++ src/main/java/com/example/DecretandoViajesAPP/services/UserService.java
@@ -4,7 +4,9 @@ import com.example.DecretandoViajesAPP.models.User;
 import com.example.DecretandoViajesAPP.repositories.IRepositorieSpace;
 import com.example.DecretandoViajesAPP.repositories.IRepositorieUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -13,26 +15,39 @@ public class UserService {
 
     //Inyectando una dependencia al repositorioUser
     @Autowired
-    private IRepositorieSpace repositorieSpace;
+    private IRepositorieSpace repositorieUser;
 
-    public boolean saveSpacesInDB(User data){}
+    public boolean saveUserInDB(User data){
+        //condiciones logicas para validar datos a guardar
 
+        if(repositorieUser.findByEmail(data.getEmail().isPresent()){
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Ya existe un correo registrado igual al que me entregas "
+            );
+        }
 
-    public boolean modifySpacesInBD(User data, UUID id){
-
-        //validar q datos me envian y si estas cumplen las reglas del negocio
-        //modificar los datos en BD
         return false;
     }
 
-    public boolean deleteSpacesInBD(UUID id){
+
+    public boolean modifyUserInBD(User data, UUID id){
+
+        //Condiciones logicas para validar datos a guardar
+
+        //1. Validar que el correo a registrar no se haya guardado previamente
+
+        return false;
+    }
+
+    public boolean deleteUserInBD(UUID id){
 
         //Buscar y validar si el ID que me envian existe
         //elimino el registro en BD
         return false;
     }
 
-    public boolean searchSpacesInBD(){
+    public boolean searchUserInBD(){
 
         //Dependiendo del parametro de busqueda debo implementar validaciones
         //Devuelve los usuarios o usuario que se encuentre en BD
